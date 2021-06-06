52771cad15f75f8fbb8db47f26e990da1348c7f9
denis <denis.da.by@gmail.com>
2021-06-06 15:56:17 +0300

    add eslint-plugin-unicorn

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:import/recommended',
     'plugin:sonarjs/recommended',
     'plugin:promise/recommended',
+    'plugin:unicorn/recommended',
     'airbnb',
     'airbnb/hooks',
   ],
@@ -23,5 +24,8 @@ module.exports = {
   rules: {
     'no-new-wrappers': 'error',
     'default-param-last': 'error',
+    'unicorn/no-null': 'off',
+    'unicorn/prefer-module': 'off',
+    'no-use-before-define': ['error', { functions: false, classes: false }],
   },
 };
